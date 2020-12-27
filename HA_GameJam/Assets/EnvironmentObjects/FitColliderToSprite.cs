236222af53c3cb246276d0d72f6fd122ebd3fc13
liamgu3 <43424244+liamgu3@users.noreply.github.com>
2020-12-27 13:48:49 -0600

    Implemented Movement, Collisions, and Basic Animation

---
+++ HA_GameJam/Assets/EnvironmentObjects/FitColliderToSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitColliderToSprite : MonoBehaviour
+{
+	//this script automatically sets an objects collider to the size of its sprite
+
+	private SpriteRenderer renderer;    //the object's sprite renderer
+	private BoxCollider2D collider;		//the object's box collider
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		renderer = GetComponent<SpriteRenderer>();
+		collider = GetComponent<BoxCollider2D>();
+
+		collider.size = renderer.bounds.size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ HA_GameJam/Assets/Player/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	//used for choosing which animation to use
+	private Animator animator;
+	private int direction;
+
+	private float speed;	//speed that character moves at
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		animator = GetComponent<Animator>();
+		direction = 0;
+
+		speed = .05f;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+	private void FixedUpdate()
+	{
+		Movement();
+	}
+
+	//moves player and assigns correct animation
+	private void Movement()
+	{
+		if (Input.GetKey(KeyCode.S))    //walking down
+		{
+			transform.position = new Vector2(transform.position.x, transform.position.y - speed);
+			direction = 0;
+		}
+		else if (Input.GetKey(KeyCode.W))   //walking up
+		{
+			transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+			direction = 1;
+		}
+
+		if (Input.GetKey(KeyCode.D))    //walking right
+		{
+			transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+			direction = 2;
+		}
+		else if (Input.GetKey(KeyCode.A))   //walking up
+		{
+			transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+			direction = 3;
+		}
+
+		animator.SetInteger("Direction", direction);
+	}
+}
