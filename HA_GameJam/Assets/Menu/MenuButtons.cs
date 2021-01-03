ccb624dbf7bdd454fc37c583e4467fdc02697f29
liamgu3 <43424244+liamgu3@users.noreply.github.com>
2021-01-03 13:22:25 -0600

    implemented both videos

--- HA_GameJam/Assets/Menu/MenuButtons.cs
+++ HA_GameJam/Assets/Menu/MenuButtons.cs
@@ -6,26 +6,59 @@ using UnityEngine.Video;
 
 public class MenuButtons : MonoBehaviour
 {
+	private bool videoStarted;
+	private VideoPlayer video;
+
+	public GameObject background;
+	public GameObject start;
+	public GameObject exit;
+	public GameObject skip;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		videoStarted = false;
+		video = GetComponent<VideoPlayer>();
+
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+		if (videoStarted)
+		{
+			if (!video.isPlaying)
+			{
+				SceneManager.LoadScene(1);
+			}
+		}
+
     }
 
 	public void StartGame()
 	{
-		GetComponent<VideoPlayer>().Play();
-		//SceneManager.LoadScene(1);
+		//GameObject.Find("Canvas").SetActive(false);
+		GetComponent<AudioSource>().Stop();
+		video.Play();
+		videoStarted = true;
+		ChangeUI();
 	}
 
 	public void ExitGame()
 	{
 		Application.Quit();
+	}
+
+	public void ChangeUI()
+	{
+		background.SetActive(false);
+		start.SetActive(false);
+		exit.SetActive(false);
+		skip.SetActive(true);
+	}
+
+	public void Skip()
+	{
+		video.Stop();
 	}
 }
--- HA_GameJam/Assets/ReachedExit.cs
+++ HA_GameJam/Assets/ReachedExit.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class ReachedExit : MonoBehaviour
 {
+	public GameObject camera1;
+	public GameObject blackScreen;
+	public GameObject restart;
+	public GameObject exit;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +25,28 @@ public class ReachedExit : MonoBehaviour
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.tag == "Player")
-		{ 
-			//play end game thing
+		{
+			camera1.GetComponents<AudioSource>()[0].Stop();
+			camera1.GetComponents<AudioSource>()[1].Stop();
+			GameObject.Find("Player").GetComponent<Player>().movementPause = true;
+			GetComponent<VideoPlayer>().Play();
+			ChangeUI();
 		}
+	}
+
+	public void ChangeUI()
+	{
+		Transform canvas1 = GameObject.Find("Canvas").transform;
+
+		for (int i = 0; i < canvas1.childCount; i++)
+		{
+			canvas1.GetChild(i).gameObject.SetActive(false);
+		}
+
+		blackScreen.SetActive(true);
+		restart.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -250, 0);
+		exit.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -350, 0);
+		restart.SetActive(true);
+		exit.SetActive(true);
 	}
 }
